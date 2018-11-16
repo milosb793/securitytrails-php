245d5a5edeb3208ca9e8b97b02371ae7411d5e86
Milos <milosb793@gmail.com>
2018-11-16 21:45:07 +0100

    Add Api classes sample

---
+++ src/SecurityTrails/Api/DomainApi.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SecurityTrails\Api;
+
+class DomainApi
+{
+
+}
---
+++ src/SecurityTrails/Api/SecurityTrailsAbstractApi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SecurityTrails\Api;
+
+class SecurityTrailsAbstractApi
+{
+    private $api_base_url;
+    private $api_version = '1';
+
+}
